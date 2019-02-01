aaafa84855cb67b8bc3ac84dd3135c64571782e9
Pauls Bebris <pauls.bebris@gmail.com>
2019-02-01 15:49:50 +0200

    homework song.js

---
+++ song.js
@@ -0,0 +1,49 @@
+var song = 'Beautiful Earth';
+var artist = 'Nice Girl';
+var genre = 'grunge';
+var year = 2019;
+var awards = [{
+    grammy: {
+        year: 2019,
+        nomination: 'Best lirycs'
+    },
+    'coll Award': {
+        year: 2019,
+        nomination: 'Best lirycs'
+    }
+}];
+var details = {
+    producer: '',
+    songLength: 7.52,
+    instrumentsPlayed: ['guitar', 'keyboard', 'flute', 'drums', 'bongo']
+}
+var coppiesSold = 500000;
+var artistInfo = {
+    born: 2002,
+    countryOfOrigin: 'Latvia',
+    education: 'Higher musical',
+    frequentlyPerformssAt: 'Riga club X',
+    likesAnimals: true
+};
+var otherArtistsInvolved = [];
+var publicRating = 9.12;
+var isAvailableAt = ['store 1', 'store 2', 'download from A', 'download from B'];
+var concerts = {
+    berlin: {
+        when: '2019.12.12',
+        where: 'Berliner Zoo',
+        ticketPrice: '15 EUR'
+    },
+    paris: {
+        when: '2019.10.12',
+        where: 'Eifel Tower',
+        ticketPrice: '15 EUR'
+    },
+    madrid: {
+        when: '2019.8.12',
+        where: 'Spanniard Square',
+        ticketPrice: '15 EUR'
+    }
+    
+};
+var doesCharity = true;
